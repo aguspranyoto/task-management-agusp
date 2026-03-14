6c1f27c5268a020f1bfc8d45f99fd117d7328b89
aguspranyoto <agusprnyt@gmail.com>
2026-03-14 23:56:01 +0700

    feat: add ecosystem configuration and update package name

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "task-management-agusp",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
